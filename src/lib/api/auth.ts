8709aede048c3323cbc39679f26271ed4272553b
bpond37 <bpond37@gmail.com>
2020-02-05 00:34:54 +0900

    env setting

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -6,4 +6,4 @@ export const getUserTopArtists = (header: any) => client.get('/me/top/artists?ti
 
 export const getUserTopTracks = (header: any) => client.get('/me/top/tracks?time_range=medium_term&limit=12&offset=5',{headers: header})
 
-export const getUserRecentlyPlayedTracks = (header: any) => client.get('https://api.spotify.com/v1/me/player/recently-played?type=track&limit=12',{headers: header})
+export const getUserRecentlyPlayedTracks = (header: any) => client.get('/me/player/recently-played?type=track&limit=12',{headers: header})
