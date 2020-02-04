441610474100f177326bfed4a27edacd101f94ac
bpond37 <bpond37@gmail.com>
2020-02-05 00:39:09 +0900

    env setting

--- src/env.ts
+++ src/env.ts
@@ -2,5 +2,5 @@ export default {
   client_id : '31b2fc8da6db4bc3af985514b8d69d31',
   client_secret : 'f6c02bcaabb94e9aa2bb853a63a88b1c', 
   // redirect_uri :'http://localhost:3000/callback', 
-  redirect_uri :'http://rooftify.herokuapp.com//callback', 
+  redirect_uri :'https://rooftify.herokuapp.com//callback', 
 };
